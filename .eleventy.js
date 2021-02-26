ba4747c6bd4539114e996692b38f99637c22af19
Peter Broom <me@peterbroom.co.uk>
2021-02-26 17:49:17 +0000

    add html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,19 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("admin");
+
+  // Minify HTML output
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if (outputPath.indexOf(".html") > -1) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
 };
