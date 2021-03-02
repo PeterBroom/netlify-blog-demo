2d9dc4ddc6bfdb52c25cc6b226df9ef342bfc7c6
Peter Broom <me@peterbroom.co.uk>
2021-03-02 09:58:43 +0000

    footer and header nav

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
+const yaml = require("js-yaml");
 const htmlmin = require("html-minifier");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const searchFilter = require("./src/filters/searchFilter");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+    // Global collection
+    eleventyConfig.addDataExtension('yaml', contents => yaml.safeLoad(contents))
+    eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
 
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
@@ -23,7 +28,10 @@ module.exports = function (eleventyConfig) {
 
     // Search filter
     eleventyConfig.addFilter("search", searchFilter);
+
+    // Page collection
     eleventyConfig.addCollection("pages", collection => {
         return [...collection.getFilteredByGlob("./src/pages/**/*.md")];
     });
+
 };
